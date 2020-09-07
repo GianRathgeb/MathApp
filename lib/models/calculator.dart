c232ba6e5973a8da43901a027235f865a28a9ce1
gianrathgeb <gian.rathgeb@bbz-sh.ch>
2020-09-07 15:57:50 +0200

    Added model for the different calculators

---
+++ lib/models/calculator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/foundation.dart';
+
+class Calculator {
+  final String title;
+  final String screen;
+  final Object image;
+
+  Calculator({
+    @required this.title,
+    @required this.screen,
+    @required this.image,
+  });
+}
